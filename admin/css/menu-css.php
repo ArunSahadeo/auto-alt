7a00264eb95eb6e4fada86da49c5ff845e6c4249
Arun Sahadeo <arunjamessahadeo@gmail.com>
2018-08-30 23:12:19 +0100

    Hide menu item

--- admin/css/menu-css.php
+++ admin/css/menu-css.php
@@ -6,6 +6,11 @@ function addPluginMenuCSS ()
 {
 ?>
 <style id="auto-alt-menu-css">
+    li#toplevel_page_auto-alt-settings > ul.wp-submenu li.wp-first-item
+    {
+        display: none;
+    }
+
     li#toplevel_page_auto-alt-settings > a:active
     {
         pointer-events: none;
